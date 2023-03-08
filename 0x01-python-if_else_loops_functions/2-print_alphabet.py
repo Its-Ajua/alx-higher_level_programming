e9b15df4b82127beb5ca4aa6a604381da29b98ff
Its-Ajua <akinladerebecca04@gmail.com>
2023-03-07 16:58:27 -0800

    Python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    vaLUe = chr(i)
-    alPha = "" + vaLUe
-    print("{}".format(alPha), end='')
+i = 0
+while i < 26:
+    print('{:c}'. format(i + 97), end='')
+    i = i + 1
