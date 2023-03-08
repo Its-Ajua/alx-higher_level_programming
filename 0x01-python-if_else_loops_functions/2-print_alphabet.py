fed152b6614314322409fe38bd2f87b2599e5317
Its-Ajua <akinladerebecca04@gmail.com>
2023-03-07 16:48:45 -0800

    Python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
     val = chr(ch)
-    alpha = " " + val
-    print("{}".format(alpha), end="")
+    alpha = "" + val
+    print("{}".format(alpha), end='')
