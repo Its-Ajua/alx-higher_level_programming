f3136f6dcce876a4240472af3801dcfe0630d7bc
Its-Ajua <akinladerebecca04@gmail.com>
2023-03-13 15:03:23 -0700

    Python

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list = "":
+        return None
+    number = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > number:
+            number = my_list[i]
+    return (number)
