564ba1ecf9cc37cf373c869ca3c0a98ac1d1392f
Its-Ajua <akinladerebecca04@gmail.com>
2023-03-15 14:53:44 -0700

    Python

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for i in range(len(a_dictionary)):
+    if a_dictionary.get(key) is not None:
         return (a_dictionary.remove(key[i]))
