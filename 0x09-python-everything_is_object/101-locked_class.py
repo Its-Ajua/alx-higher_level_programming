d93bf6f8f0c03cb78b4d62309134dc502149dc52
root <akinladerebecca04@gmail.com>
2023-04-04 10:47:53 -0700

    Fixing errors

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Creating a LockClass class"""
+"""Defining a LockClass class"""
 
 
 class LockedClass:
-    """Defining a class that prevents the user from dynamically creating new instance
+    """prevents the user from dynamically creating new instance
     attributes, except if the new instance attribute is called first_name
     """
     
