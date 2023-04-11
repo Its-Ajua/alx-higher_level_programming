0ff5cc83ebbbb35b524e664d87e80108f1b32463
root <akinladerebecca04@gmail.com>
2023-04-11 14:49:45 -0700

    Inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,4 +6,4 @@ def is_kind_of_class(obj, a_class):
     """function that returns True if object is an instance of,
     or if the object is an instance of a class; otherwise False
     """
-    return (if isinstance(obj, a_class))
+    return (isinstance(obj, a_class))
