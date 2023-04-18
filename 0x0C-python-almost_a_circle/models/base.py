155fbdf29094bb334c16dd5e2b8c179108542997
root <akinladerebecca04@gmail.com>
2023-04-17 20:01:15 -0700

    Yay Python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,7 +16,7 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            __nb_objects += 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
     @staticmethod
